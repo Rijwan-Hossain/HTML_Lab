0adf174bd3f949367910a26cfa7eade86ad699df
Rijwan Hossain <rijyan.cse.152@gmail.com>
2019-02-26 12:24:13 -0800

    Personal website build

---
+++ Project1/lab_task/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// import logo from './logo.svg';
+// import './App.css';
+import Home from './Components/Home/Home';
+import Error from './Components/Error';
+import Gallery from './Components/Gallery/Gallery';
+import Resume from './Components/Resume/Resume';
+import Navigation from './Components/Navigation';
+
+class App extends Component { 
+   render() { 
+      return ( 
+         <BrowserRouter> 
+            <div>
+               <Navigation /> 
+               <Switch> 
+                  <Route path="/" component={Home} exact /> 
+                  <Route path="/gallery" component={Gallery} /> 
+                  <Route path="/resume" component={Resume} /> 
+                  <Route component={Error} exact /> 
+               </Switch> 
+            </div> 
+         </BrowserRouter> 
+      );
+   } 
+} 
+
+export default App;
